d633104710634b21858bff1df692dc1e79c84447
Christian <christiansmith2394@gmail.com>
2021-12-06 19:27:37 -0500

    20/26

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,60 +1,54 @@
-const Account = require('./accounts-model')
+const Accounts = require('./accounts-model')
 const db = require('../../data/db-config')
 
-exports.checkAccountPayload = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('checking payload')
-  if (!req.body.name || !req.body.budget) {
+function checkAccountPayload (req, res, next) {
+  let { name, budget } = req.body;
+  // name = name.trim();
+  
+  if (name === undefined || budget === undefined) {
     res.status(400).json({ message: 'name and budget are required' })
   }
-  else if (typeof req.body.name != 'string') {
+  else if (typeof(name) != 'string') {
     res.status(400).json({ message: "name of account must be a string" })
   }
-  else if (req.body.name.length < 3 || req.body.name.length > 100) {
+  else if (name.trim().length < 3 || name.trim().length > 100) {
     res.status(400).json({ message: "name of account must be between 3 and 100" })
   }
-  else if (typeof req.body.budget != 'number') {
+  else if (typeof budget !== 'number' || isNaN(budget)) {
     res.status(400).json({ message: "budget of account must be a number" })
   }
-  else if (req.body.budget < 0 || req.budget > 1000000) {
+  else if (budget <= 1 || budget > 1000000) {
     res.status(400).json({ message: "budget of account is too large or too small" })
   }
-
+  
   else {
     next()
   }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('checking if name unique')
+function checkAccountNameUnique (req, res, next) {
   db('accounts').where({ name: req.body.name }).first()
     .then(account => {
       if (!account) {
         next()
       } else {
-        res.status(400).json('that name is taken')
+        res.status(400).json({ message: "that name is taken" })
       }
     })
-    .catch(err => {
-      res.json({ message: err.message })
-    })
+    .catch(next)
 }
 
-exports.checkAccountId = (req, res, next) => {
-  // DO YOUR MAGIC
-  console.log('checking account id')
-  const { id } = req.params;
-  Account.getById(id)
-    .then(account => {
-      if (!account) {
-        res.status(404).json({ message: "account not found" })
-      } else {
-        next()
-      }
-    })
-    .catch(err => {
-      res.statu(500).json({ message: 'error retrieving from database' })
-    })
-
+function checkAccountId(req, res, next) {
+  const { id } = req.params
+  Accounts.getById(id).then(account => {
+    if (account) {
+      req.verified = account
+      next()
+    } else {
+      res.status(404).json({message: "account not found"})
+    }
+  })
+  
 }
+
+module.exports = { checkAccountId, checkAccountPayload, checkAccountNameUnique }
--- queries.sql
+++ queries.sql
@@ -1,29 +1,17 @@
 -- Database Queries
 
-SELECT * FROM Customers where PostalCode = 1010
 -- Find all customers with postal code 1010
-SELECT * FROM Customers where PostalCode = 1010
+SELECT * FROM Customers WHERE postalCode = "1010"
 -- Find the phone number for the supplier with the id 11
-
+SELECT Phone FROM Suppliers Where SupplierID = "11"
 -- List first 10 orders placed, sorted descending by the order date
-SELECT CustomerID as ID, Orderdate as date
-FROM Orders
-order by date
-desc limit 10
+SELECT * FROM Orders ORDER BY orderdate desc LIMIT 10
 -- Find all customers that live in London, Madrid, or Brazil
-SELECT 
-CustomerName as Name, 
-city, 
-country
-FROM Customers
-where city = 'Madrid' or city = 'London'
-or country = 'Brazil'
-
+SELECT * FROM Customers WHERE city="Berlin" OR city="London" OR Country="Brazil"
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
-
-insert into Customers (CustomerName,ContactName,Address,City,PostalCode,Country)
-values ('The Shire','bilbo baggins','hobbit hole','bag end','111','middle earth')
---Update Bilbo Baggins record so that the postal code changes to "11122"
+insert into customers (CustomerName, ContactName, Address, City, PostalCode, Country) values ("The Shire", "Bilbo Baggins", "1 Hobbit-Hole", "Bag End", "111", "Middle Earth")
+-- Update Bilbo Baggins record so that the postal code changes to "11122"
+UPDATE Customers SET postalcode="11122" WHERE customerID=92
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
